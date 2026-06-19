9148ad293bc7938a189b7792028cd4a6af41f718
Seif El-den Karam salah <sk1536@fayoum.edu.eg>
2026-06-19 23:24:31 +0300

    authentication V2

--- MediCare.Web/ViewModels/LoginRequest.cs
+++ MediCare.Web/ViewModels/LoginRequest.cs
@@ -2,7 +2,7 @@
 {
     public class LoginRequest
     {
-        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
         public bool Rememberme {  get; set; }
     }

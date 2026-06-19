f0fe0ad67d55934ed8d3a6f0b57b766b4ee2bc83
Seif El-den Karam salah <sk1536@fayoum.edu.eg>
2026-06-19 17:22:49 +0300

    fix in the authentication

--- MediCare.Data/Models/ApplicationUser.cs
+++ MediCare.Data/Models/ApplicationUser.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace MediCare.Data.Models
 {
     public class ApplicationUser:IdentityUser
     {
-        public string FullName { get; set; }
-
+        public string FullName { get; set; } = null!;
         public Gender Gender { get; set; }
-
         public Doctor? DoctorProfile { get; set; }
         public Patient? PatientProfile { get; set; }
 
--- MediCare.Web/ViewModels/RegisterRequest.cs
+++ MediCare.Web/ViewModels/RegisterRequest.cs
@@ -5,6 +5,7 @@ namespace MediCare.Web.ViewModels
     public class RegisterRequest
     {
         public string Name { get; set; } = null!;
+        public string Email { get; set; }=null!;
         public Gender Gender { get; set; }
         public string Role {  get; set; }=null!;
         public string Password {  get; set; } = null!;

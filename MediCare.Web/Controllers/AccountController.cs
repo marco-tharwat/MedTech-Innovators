cb4ee63416c6038210ef8724be43730332909131
Seif El-den Karam salah <sk1536@fayoum.edu.eg>
2026-07-04 22:59:06 +0300

    fix the Logout

--- MediCare.Web/Controllers/AccountController.cs
+++ MediCare.Web/Controllers/AccountController.cs
@@ -107,7 +107,7 @@ namespace MediCare.Web.Controllers
             return View("Login");
         }
 
-        [HttpGet]
+        [HttpPost]
         public async Task<ActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
